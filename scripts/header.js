ef0f066cbd695b361469e158c4af9ee6a7751fd2
lucian.paslariu <lucian.paslariu@cgm.com>
2017-11-28 09:29:53 +0200

    first commit

---
+++ scripts/header.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+    <div id=\"hgroup\">
+      <h1><a href=\"#\">Basic 87</a></h1>
+      <h2>Free HTML5 Website Template</h2>
+    </div>
+    <nav>
+      <ul>
+        <li><a href=\"index.html\">Home</a></li>
+        <li><a href=\"infographicType.html\">About</a></li>
+        <li><a href=\"/contact.html\">Contact</a></li>
+        <li><a href=\"#\">Sign Up</a></li>
+          <!-- signup will have a popup -->
+        <li class=\"last\"><a href=\"/login.html\">Login</a></li>
+      </ul>
+    </nav>            
+');
